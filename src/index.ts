5e06e97edd0828b50621d3d1203b8b95c07265bf
charbelyounes622-tech <charbelyounes622@gmail.com>
2026-07-29 13:33:20 +0300

    Use compatible Cloudflare asset type

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { onRequestPost as createOrder } from "../functions/api/orders";
 import { onRequestGet as listOrders } from "../functions/api/admin-orders";
 
 interface Env {
-  ASSETS: Fetcher;
+  ASSETS: { fetch(request: Request): Promise<Response> };
   SUPABASE_URL: string;
   SUPABASE_SERVICE_ROLE_KEY: string;
   ADMIN_EMAIL: string;
